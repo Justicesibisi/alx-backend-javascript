4347d65b4ccc6b7648a4bd6d7fe75dfe725fc9bf
Justicesibisi <djmangsa@gmail.com>
2024-10-10 23:12:30 +0200

    To be continued

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,16 +1,25 @@
-function updateStudentGradeByCity(students, city, newGrades) {
-  return students
-    .filter(student => student.location === city)  // Step 1: Filter students by the city
-    .map(student => {
-      // Step 2: Find the new grade for the student by matching studentId
-      const studentGrade = newGrades.find(gradeObj => gradeObj.studentId === student.id);
-      
-      // Step 3: Return the student object with the updated grade or 'N/A'
-      return {
-        ...student,
-        grade: studentGrade ? studentGrade.grade : 'N/A',  // Add the grade or set 'N/A'
-      };
-    });
-}
+export default function updateStudentGradeByCity(
+  students,
+  city,
+  newGrades,
+) {
+  const filterStudents = students.filter(
+    (student) => student.location === city,
+  );
 
-export default updateStudentGradeByCity;
+  const gradeStudents = filterStudents.map(
+    (student) => {
+      for (const gradeInfo of newGrades) {
+        if (student.id === gradeInfo.studentId) {
+          student.grade = gradeInfo.grade; // eslint-disable-line no-param-reassign
+        }
+      }
+      if (student.grade === undefined) {
+        student.grade = 'N/A'; // eslint-disable-line no-param-reassign
+      }
+      return student;
+    },
+  );
+
+  return gradeStudents;
+}
